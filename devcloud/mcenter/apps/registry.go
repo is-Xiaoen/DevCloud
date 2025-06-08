c9a4937330f99e3540a4e56e43346b7fdcbd9303
xiaoen <2768753269@qq.com>
2025-06-08 12:04:18 +0800

    补充endpoint模块

--- devcloud/mcenter/apps/registry.go
+++ devcloud/mcenter/apps/registry.go
@@ -7,6 +7,8 @@ import (
 	_ "122.51.31.227/go-course/go18/devcloud/mcenter/apps/token/api"
 	_ "122.51.31.227/go-course/go18/devcloud/mcenter/apps/token/impl"
 
+	_ "122.51.31.227/go-course/go18/devcloud/mcenter/apps/endpoint/impl"
+
 	// 颁发器
 	_ "122.51.31.227/go-course/go18/devcloud/mcenter/apps/token/issuers"
 	// 鉴权中间件
