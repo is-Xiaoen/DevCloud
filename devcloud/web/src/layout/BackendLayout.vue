8a3d698a4edc4453ea564303b85877a56111ec2a
xiaoen <2768753269@qq.com>
2025-08-03 12:07:18 +0800

    补充工程初始化

---
+++ devcloud/web/src/layout/BackendLayout.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    后台布局页面
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="css" scoped></style>
---
+++ devcloud/web/src/router/index.js
@@ -0,0 +1,24 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+import HomePage from '@/pages/HomePage.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomePage,
+    },
+    {
+      path: '/login',
+      name: 'login',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../pages/LoginPage.vue'),
+    },
+  ],
+})
+
+export default router
